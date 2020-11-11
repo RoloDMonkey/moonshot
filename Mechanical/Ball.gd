8561f7c92308e4303058c3530faafd1fc0c22527
Rolo D. Monkey <rolo@irolo.net>
2020-11-10 21:58:08 -0500

    - cleaning up scripts - setting default z-index of bumpers to 1 - stopping work on shooter test until tomorrow

--- Mechanical/Ball.gd
+++ Mechanical/Ball.gd
@@ -1,22 +1,10 @@
 extends KinematicBody2D
 
-
-# Declare member variables here. Examples:
-# var a = 2
-# var b = "text"
+# Ball
 
 export (int) var gravity = 150
 
 var velocity = Vector2()
-
-# Called when the node enters the scene tree for the first time.
-func _ready():
-	pass # Replace with function body.
-
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(delta):
-#	pass
 
 func _draw():
 	draw_circle(Vector2(0,0), 13.5, Color(ColorN("red", 1)))
@@ -26,8 +14,8 @@ func _physics_process(delta):
 	
 	var collision = move_and_collide(velocity * delta)
 	if collision:
-		var speed = 1;
-		if collision.collider.has_method("get_push_multiplier"):
-			speed = collision.collider.get_push_multiplier()
-		velocity = collision.normal * speed;
-		
+		if collision.collider.has_method("get_push_speed"):
+			var push_speed = collision.collider.get_push_speed()
+			velocity = collision.normal * push_speed
+		else:
+			velocity = velocity.bounce(collision.normal)
--- Mechanical/Shooter.gd
+++ Mechanical/Shooter.gd
@@ -1,19 +1,6 @@
-extends Node2D
+extends StaticBody2D
 
-
-# Declare member variables here. Examples:
-# var a = 2
-# var b = "text"
-
-
-# Called when the node enters the scene tree for the first time.
-func _ready():
-	pass # Replace with function body.
-
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(delta):
-#	pass
+# Shooter
 
 func _draw():
 	var line_color = Color(ColorN("blue", 1))
@@ -25,3 +12,6 @@ func _draw():
 	draw_line(Vector2(1.5, 85), Vector2(1.5, 40), line_color, 1.0, true)
 	draw_arc(Vector2(0, -46.5), 12.5, PI * -1, 0, 25, line_color, 1.0, true)
 	draw_arc(Vector2(0, 40), 1.5, PI * -1, 0, 3, line_color, 1.0, true)
+	
+#func get_bounce():
+#	return self.bounce
