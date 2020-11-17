04b00f6c52967dcd8b7f8fbc036febfe281c5c38
Rolo D. Monkey <rolo@irolo.net>
2020-11-16 19:21:00 -0500

    - added shoot input - added input event listener and shoot function to shooter - tweaked shooter and plunger collision objects to guide plunger straight

--- Mechanical/Plunger.gd
+++ Mechanical/Plunger.gd
@@ -7,14 +7,11 @@ var shooting = false
 
 func _draw():
 	draw_circle(Vector2(0,-9), 6, Color(ColorN("blue", 1)))
-	draw_rect(Rect2(-6, -9, 12, 24), Color(ColorN("blue", 1)))
+	draw_rect(Rect2(-6, -9, 12, 36), Color(ColorN("blue", 1)))
 
 func _input(event):
-	if event.is_action_pressed("ui_accept"):
-		self.shooting = true
-
-func _integrate_forces(state):
-	if self.shooting:
-		self.apply_central_impulse(Vector2(0, -1) * plunger_speed)
-		self.shooting = false
-	
+	if event.is_action_pressed("shoot"):
+		self.shoot()
+		
+func shoot():
+	self.apply_central_impulse(Vector2(0, -1) * plunger_speed)
--- Mechanical/Shooter.gd
+++ Mechanical/Shooter.gd
@@ -12,4 +12,3 @@ func _draw():
 	draw_line(Vector2(1.5, 85), Vector2(1.5, 40), line_color, 1.0, true)
 	draw_arc(Vector2(0, -46.5), 12.5, PI * -1, 0, 25, line_color, 1.0, true)
 	draw_arc(Vector2(0, 40), 1.5, PI * -1, 0, 3, line_color, 1.0, true)
-	
