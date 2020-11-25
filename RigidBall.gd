ad7c3181b520ae8b3182bbcd8c045433855fc492
Andrew Predmore <apredmore@thirdandgrove.com>
2020-11-24 20:18:34 -0500

    - added folder for image assets - added ball and playfield images - copied ball image for icon - moved RigidBall (pinball) out of mechanical folder - added ball image as sprite to pinball - got rid of left flipper test, for now - added left flipper mechanical scene

--- RigidBall.gd
+++ RigidBall.gd
@@ -2,9 +2,6 @@ extends RigidBody2D
 
 # RigidBall
 
-func _draw():
-	draw_circle(Vector2(0,0), 13.5, Color(ColorN("orange", 1)))
-	
 func _integrate_forces(state):
 	for i in range(state.get_contact_count()):
 		var o = state.get_contact_collider_object(i)
@@ -12,4 +9,3 @@ func _integrate_forces(state):
 			var push_speed = o.get_push_speed()
 			var contact_normal = state.get_contact_local_normal(i).normalized()
 			self.apply_central_impulse(contact_normal * push_speed)
-
