27532acd2d8497d07fec3f3183dee9bcdeb93039
Andrew Predmore <apredmore@thirdandgrove.com>
2020-11-26 08:31:29 -0500

    - flipper movement is working; saving before fixing flipper collisions

--- Mechanical/Left Flipper.gd
+++ Mechanical/Left Flipper.gd
@@ -7,7 +7,7 @@ var flipping = false
 var returning = false
 var flip_time = 0
 var flipper
-var up_angle = deg2rad(-24.7)
+var up_angle = deg2rad(-34.7)
 var down_angle = deg2rad(30.3)
 
 func _ready():
@@ -22,23 +22,16 @@ func _input(event):
 func flip():
 	self.returning = false
 	if (!self.flipping):
-		print_debug("flipping")
 		self.flipping = true
-		var current_offset = abs(self.flipper.rotation_degrees - self.down_angle)
-		var debug_string = "current_offset: %s"
-		print_debug(debug_string % current_offset)
+		var current_offset = abs(deg2rad(self.flipper.rotation_degrees) - self.down_angle)
 		var total_offset = abs(self.up_angle - self.down_angle)
-		debug_string = "total_offset: %s"
-		print_debug(debug_string % total_offset)
 		self.flip_time = (current_offset / total_offset) * self.flip_duration
-		debug_string = "flip_time: %s"
-		print_debug(debug_string % self.flip_time)
 	
 func release():
 	self.flipping = false
 	if (!self.returning):
 		self.returning = true
-		var current_offset = abs(self.up_angle - self.flipper.rotation_degrees)
+		var current_offset = abs(self.up_angle - deg2rad(self.flipper.rotation_degrees))
 		var total_offset = abs(self.up_angle - self.down_angle)
 		self.flip_time = (current_offset / total_offset) * self.flip_duration
 
@@ -46,15 +39,15 @@ func _physics_process(delta):
 	if (self.flipping):
 		if (self.flip_time < self.flip_duration):
 			var weight = self.flip_time / self.flip_duration
-			self.flipper.rotation_degrees = lerp_angle(self.down_angle, self.up_angle, weight)
+			self.flipper.rotation_degrees = rad2deg(lerp_angle(self.down_angle, self.up_angle, weight))
 			self.flip_time += delta
 		else:
 			self.flipping = false
 	elif (self.returning):
 		if (self.flip_time < self.flip_duration):
 			var weight = self.flip_time / self.flip_duration
-			self.flipper.rotation_degrees = lerp_angle(self.up_angle, self.down_angle, weight)
-			self.flip_time + delta
+			self.flipper.rotation_degrees = rad2deg(lerp_angle(self.up_angle, self.down_angle, weight))
+			self.flip_time += delta
 		else:
 			self.returning = false
 	
